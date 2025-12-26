f27720b7aa0fd267c9a8898511868f737b2c010d
Valdur Kana <valdur55@gmail.com>
2025-12-26 15:45:51 +0200

    bugfix for rofi-433.sh runtime error

--- bin/rofi-433.sh
+++ bin/rofi-433.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-options=$(awk -F"," '{gsub(/\"/,""); gsub(/ /, " ") ; print$1}' shortcuts.csv | tail -n +2)
+echo $PWD
+options=$(awk -F"," '{gsub(/\"/,""); gsub(/ /, " ") ; print$1}' ~/arendus/valdur-433/shortcuts.csv | tail -n +2)
 chosen="$(echo -e "$options" | rofi -dmenu)"
 
 if [[ -z $choosen ]]
